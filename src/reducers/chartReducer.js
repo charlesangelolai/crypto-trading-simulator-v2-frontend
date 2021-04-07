1b8cf27d350c2bb43b1053826d50bb6f12e4eabf
Charles Lai <charlesangeloplai@gmail.com>
2021-04-06 20:43:53 -0400

    adds chart reducer

---
+++ src/reducers/chartReducer.js
@@ -0,0 +1,31 @@
+const initialState = {
+  loading: true,
+  coin: null,
+  chartData: [],
+};
+
+const chartReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "LOADING":
+      return {
+        ...state,
+        loading: true,
+      };
+    case "FETCH_COIN_DATA":
+      return {
+        ...state,
+        loading: false,
+        coin: action.payload,
+      };
+    case "FETCH_CHART_DATA":
+      return {
+        ...state,
+        loading: false,
+        chartData: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default chartReducer;
