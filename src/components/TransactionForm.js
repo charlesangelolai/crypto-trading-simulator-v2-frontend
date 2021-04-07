aaa5a046549ad3bc301e5515e79794964c7c1824
Charles Lai <charlesangeloplai@gmail.com>
2021-04-06 20:42:39 -0400

    adds TransactionForm.js

--- src/components/TransactionForm.js
+++ src/components/TransactionForm.js
@@ -4,10 +4,9 @@ import TextField from "@material-ui/core/TextField";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 
-const TransactionForm = ({ qty_id }) => {
+const TransactionForm = ({ coin_id, coin_name }) => {
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -16,6 +15,10 @@ const TransactionForm = ({ qty_id }) => {
 
   const handleClose = () => {
     setOpen(false);
+  };
+
+  const handleSubmit = (e) => {
+    debugger;
   };
 
   return (
@@ -28,26 +31,28 @@ const TransactionForm = ({ qty_id }) => {
         onClose={handleClose}
         aria-labelledby="form-dialog-title"
       >
-        <DialogTitle id="form-dialog-title">Enter Amount:</DialogTitle>
-        <DialogContent>
-          {/* <DialogContentText>Enter Amount:</DialogContentText> */}
-          <TextField
-            autoFocus
-            margin="dense"
-            id="qty"
-            label={qty_id}
-            type="number"
-            fullWidth
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleClose} color="primary">
-            Buy
-          </Button>
-        </DialogActions>
+        <form onSubmit={handleSubmit}>
+          <DialogTitle id="form-dialog-title">Buy {coin_name}</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              id={coin_id}
+              name="qty"
+              label="Enter Amount: "
+              type="number"
+              fullWidth
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button type="submit" color="primary">
+              Buy
+            </Button>
+          </DialogActions>
+        </form>
       </Dialog>
     </div>
   );
