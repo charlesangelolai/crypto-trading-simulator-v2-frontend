f93fb27fe9bee38ca990a24da7d9ed17298fe505
Charles Lai <charlesangeloplai@gmail.com>
2021-04-07 22:21:17 -0400

    adds users, trades, and positions reducers to rootReducer

--- src/reducers/combineReducer.js
+++ src/reducers/combineReducer.js
@@ -1,10 +1,16 @@
 import { combineReducers } from "redux";
 import marketReducer from "./marketReducer";
 import chartReducer from "./chartReducer";
+import usersReducer from "./usersReducer";
+import tradesReducer from "./tradesReducer";
+import positionsReducer from "./positionsReducer";
 
 const rootReducer = combineReducers({
   market: marketReducer,
   chart: chartReducer,
+  user: usersReducer,
+  trades: tradesReducer,
+  positions: positionsReducer,
 });
 
 export default rootReducer;
