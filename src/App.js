efe6fca27749cc904add9feb63cac9dae81886cd
Charles Lai <charlesangeloplai@gmail.com>
2021-04-09 20:31:15 -0400

    adds template for app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { useSelector } from "react-redux";
 import Dashboard from "./components/Dashboard";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
 const App = () => {
-  return (
-    <div className="App">
-      <Dashboard />
-    </div>
-  );
+  const user = useSelector((state) => state.user.userData);
+
+  if (user) {
+    return (
+      <div className="App">
+        <Dashboard />
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <SignUp />;
+      </div>
+    );
+  }
 };
 
 export default App;
