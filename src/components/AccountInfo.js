0d7cfe35690d587644676aba849007e02f3b9979
Charles Lai <charlesangeloplai@gmail.com>
2021-04-11 11:43:19 -0400

    removes useSelector hook

--- src/components/AccountInfo.js
+++ src/components/AccountInfo.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useSelector } from "react-redux";
 import Link from "@material-ui/core/Link";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
