2c20f33861ff0cf93d4a1a98e54ad8206df44a8c
Charles Lai <charlesangeloplai@gmail.com>
2021-04-11 11:32:15 -0400

    adds react routers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useSelector } from "react-redux";
-import Dashboard from "./components/Dashboard";
+import Layout from "./components/Layout";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import TransactionsTable from "./components/TransactionsTable";
 
 const App = () => {
   const user = useSelector((state) => state.user.userData);
@@ -9,14 +12,28 @@ const App = () => {
   if (user) {
     return (
       <div className="App">
-        <Dashboard />
+        <Router>
+          <Layout user={user}>
+            <Switch>
+              <Route exact path="/dashboard" component={Dashboard} />
+              {/* <Route path="/news"></Route> */}
+              <Route exact path="/transactions" component={TransactionsTable} />
+              {/* <Route path="/account" exact component={Account} /> */}
+            </Switch>
+          </Layout>
+        </Router>
       </div>
     );
   } else {
     return (
-      <div className="App">
-        <SignUp />;
-      </div>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path="/signin" component={SignIn} />
+            <Route exact path="/signup" component={SignUp} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 };
