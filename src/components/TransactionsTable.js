9a737f911e32017f3cf87dceaad125ca55a3bb3c
Charles Lai <charlesangeloplai@gmail.com>
2021-04-19 17:16:36 -0400

    adds search bar feature

--- src/components/TransactionsTable.js
+++ src/components/TransactionsTable.js
@@ -1,17 +1,71 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   makeStyles,
+  fade,
   Table,
   TableBody,
   TableCell,
   TableHead,
   TableRow,
+  InputBase,
 } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
 import Title from "./Title";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserTrades } from "../actions";
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    backgroundColor: "rgba(255, 255, 255, 0.15)",
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
   seeMore: {
     marginTop: theme.spacing(3),
   },
@@ -32,6 +86,7 @@ const TransactionsList = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
   const trades = useSelector((state) => state.trades.trades);
+  const [search, setSearch] = useState("");
 
   // useEffect(() => {
   //   dispatch(getUserTrades(user.id));
@@ -39,7 +94,25 @@ const TransactionsList = () => {
 
   return (
     <React.Fragment>
-      <Title>Transactions Table</Title>
+      <div className={classes.root}>
+        <div className={classes.title}>
+          <Title>Transactions Table</Title>
+        </div>
+        <div className={classes.search}>
+          <div className={classes.searchIcon}>
+            <SearchIcon />
+          </div>
+          <InputBase
+            placeholder="Search…"
+            classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput,
+            }}
+            inputProps={{ "aria-label": "search" }}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
+      </div>
       <Table size="small">
         <TableHead>
           <TableRow>
@@ -51,24 +124,34 @@ const TransactionsList = () => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {trades.map((trade, idx) => (
-            <TableRow key={idx}>
-              <TableCell className={classes.coin}>
-                <img src={trade.logo} className={classes.logo}></img>{" "}
-                {trade.coin_name}
-              </TableCell>
-              <TableCell>{trade.symbol.toUpperCase()}</TableCell>
-              <TableCell>{trade.transaction_type}</TableCell>
-              <TableCell>{trade.qty}</TableCell>
-              <TableCell>
-                $
-                {parseFloat(trade.cost).toLocaleString(undefined, {
-                  minimumFractionDigits: 2,
-                  maximumFractionDigits: 2,
-                })}
-              </TableCell>
-            </TableRow>
-          ))}
+          {trades
+            .filter((val) => {
+              if (search === "") {
+                return val;
+              } else if (
+                val.coin_name.toLowerCase().includes(search.toLowerCase())
+              ) {
+                return val;
+              }
+            })
+            .map((trade, idx) => (
+              <TableRow key={idx}>
+                <TableCell className={classes.coin}>
+                  <img src={trade.logo} className={classes.logo}></img>{" "}
+                  {trade.coin_name}
+                </TableCell>
+                <TableCell>{trade.symbol.toUpperCase()}</TableCell>
+                <TableCell>{trade.transaction_type}</TableCell>
+                <TableCell>{trade.qty}</TableCell>
+                <TableCell>
+                  $
+                  {parseFloat(trade.cost).toLocaleString(undefined, {
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2,
+                  })}
+                </TableCell>
+              </TableRow>
+            ))}
         </TableBody>
       </Table>
     </React.Fragment>
--- src/components/WalletTable.js
+++ src/components/WalletTable.js
@@ -1,20 +1,74 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { makeStyles } from "@material-ui/core/styles";
 import {
+  makeStyles,
+  fade,
   Table,
   TableBody,
   TableCell,
   TableHead,
   TableRow,
   IconButton,
+  InputBase,
 } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
 import ShowChartIcon from "@material-ui/icons/ShowChart";
 import Title from "./Title";
 import TransactionForm from "./TransactionForm";
 import { fetchChart, getUserPositions } from "../actions";
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    backgroundColor: "rgba(255, 255, 255, 0.15)",
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
   seeMore: {
     marginTop: theme.spacing(3),
   },
@@ -42,6 +96,7 @@ const WalletList = ({ coins }) => {
   const dispatch = useDispatch();
   const positions = useSelector((state) => state.positions.positions);
   const user = useSelector((state) => state.user.userData);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     dispatch(getUserPositions(user.id));
@@ -57,7 +112,25 @@ const WalletList = ({ coins }) => {
 
   return (
     <React.Fragment>
-      <Title>CTS Wallet</Title>
+      <div className={classes.root}>
+        <div className={classes.title}>
+          <Title>CTS Wallet</Title>
+        </div>
+        <div className={classes.search}>
+          <div className={classes.searchIcon}>
+            <SearchIcon />
+          </div>
+          <InputBase
+            placeholder="Search…"
+            classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput,
+            }}
+            inputProps={{ "aria-label": "search" }}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
+      </div>
       <Table size="small">
         <TableHead>
           <TableRow>
@@ -71,63 +144,73 @@ const WalletList = ({ coins }) => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {positions.map((position, idx) => (
-            <TableRow key={idx}>
-              <TableCell className={classes.coin}>
-                <img src={position.logo} className={classes.logo}></img>{" "}
-                {position.coin_name}
-                <IconButton id={position.coin_id} onClick={handleClick}>
-                  <ShowChartIcon id={position.coin_id} />
-                </IconButton>
-              </TableCell>
-              <TableCell>{position.symbol.toUpperCase()}</TableCell>
-              <TableCell>{position.qty}</TableCell>
-              <TableCell>
-                $
-                {parseFloat(position.cost).toLocaleString(undefined, {
-                  minimumFractionDigits: 2,
-                  maximumFractionDigits: 2,
-                })}
-              </TableCell>
-              <TableCell>
-                $
-                {calculateMarketValue(
-                  position.coin_id,
-                  position.qty
-                ).toLocaleString(undefined, {
-                  minimumFractionDigits: 2,
-                  maximumFractionDigits: 2,
-                })}
-              </TableCell>
-              <TableCell>
-                <span
-                  className={
-                    calculateMarketValue(position.coin_id, position.qty) -
-                      position.cost >
-                    0.0
-                      ? classes.green
-                      : classes.red
-                  }
-                >
+          {positions
+            .filter((val) => {
+              if (search === "") {
+                return val;
+              } else if (
+                val.coin_name.toLowerCase().includes(search.toLowerCase())
+              ) {
+                return val;
+              }
+            })
+            .map((position, idx) => (
+              <TableRow key={idx}>
+                <TableCell className={classes.coin}>
+                  <img src={position.logo} className={classes.logo}></img>{" "}
+                  {position.coin_name}
+                  <IconButton id={position.coin_id} onClick={handleClick}>
+                    <ShowChartIcon id={position.coin_id} />
+                  </IconButton>
+                </TableCell>
+                <TableCell>{position.symbol.toUpperCase()}</TableCell>
+                <TableCell>{position.qty}</TableCell>
+                <TableCell>
                   $
-                  {(
-                    calculateMarketValue(position.coin_id, position.qty) -
-                    position.cost
+                  {parseFloat(position.cost).toLocaleString(undefined, {
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2,
+                  })}
+                </TableCell>
+                <TableCell>
+                  $
+                  {calculateMarketValue(
+                    position.coin_id,
+                    position.qty
                   ).toLocaleString(undefined, {
                     minimumFractionDigits: 2,
                     maximumFractionDigits: 2,
                   })}
-                </span>
-              </TableCell>
-              <TableCell align="right">
-                <TransactionForm
-                  transaction_type={"Sell"}
-                  id={position.id}
-                  name={position.coin_name}
-                />
-              </TableCell>
-            </TableRow>
-          ))}
+                </TableCell>
+                <TableCell>
+                  <span
+                    className={
+                      calculateMarketValue(position.coin_id, position.qty) -
+                        position.cost >
+                      0.0
+                        ? classes.green
+                        : classes.red
+                    }
+                  >
+                    $
+                    {(
+                      calculateMarketValue(position.coin_id, position.qty) -
+                      position.cost
+                    ).toLocaleString(undefined, {
+                      minimumFractionDigits: 2,
+                      maximumFractionDigits: 2,
+                    })}
+                  </span>
+                </TableCell>
+                <TableCell align="right">
+                  <TransactionForm
+                    transaction_type={"Sell"}
+                    id={position.id}
+                    name={position.coin_name}
+                  />
+                </TableCell>
+              </TableRow>
+            ))}
         </TableBody>
       </Table>
     </React.Fragment>
