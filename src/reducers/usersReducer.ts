c7cf25c583c4e727b80e63d04737adb959839b55
Charles Lai <charlesangeloplai@gmail.com>
2021-04-24 20:57:25 -0400

    converts to typescript

---
+++ src/reducers/usersReducer.ts
@@ -0,0 +1,22 @@
+const initialState = {
+  userData: null,
+};
+
+const usersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        userData: action.payload,
+      };
+    case "UPDATE_USER":
+      return {
+        ...state,
+        userData: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default usersReducer;
